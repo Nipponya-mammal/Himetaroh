c04b1a01c982e4bfae7ac4786f7563d5dea16ae7
Nippon-ya Channel <koheix0730@gmail.com>
2020-05-22 11:02:05 +0900

    opacity and anime modified.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ $(function() {
 
   $(".type").click(
     function(){
-    $(".ask2").fadeIn(6000);
+    $(".ask2").fadeIn(4000);
     $(".content").fadeOut();
    }); 
 
    $(".click2").click(
     function(){
     $(".ask2").fadeOut();
-    $(".content").fadeIn(5000);
+    $(".content").fadeIn(3000);
     
    }); 
 
